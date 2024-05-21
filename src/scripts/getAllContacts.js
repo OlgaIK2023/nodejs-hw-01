d4ea62fbb8fe8505833c97fc3dc030b5b15bc4b7
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-05-21 15:42:55 +0300

    Update getAllContacts.js

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+    try {
+        // Read the contacts from the database file
+        const data = await fs.readFileSync(PATH_DB, 'utf8');
+        return JSON.parse(data);
+      } catch (err) {
+        console.error('Error reading database file:', err);
+        return [];
+      }
+};
 
 console.log(await getAllContacts());
